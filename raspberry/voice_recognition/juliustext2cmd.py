e50f39b9b9f8d07dc24f051313bee7337db4c334
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-11-10 17:14:35 -0800

    Voice2text
    Succeeded to run playing music!!

--- raspberry/voice_recognition/juliustext2cmd.py
+++ raspberry/voice_recognition/juliustext2cmd.py
@@ -1,16 +1,49 @@
 #!/usr/bin/python
 
-import socket
+import subprocess
+from subprocess import Popen
+import sys
+import pyjulius
+import Queue
 
 
 HOST = 'localhost'
 PORT = 10500
 
-clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsock.connect((HOST, PORT))
+client = pyjulius.Client(HOST, PORT)
+client.connect()
+client.start()
 
-sf = clientsock.makefile('rb')
 
-while True:
-    line = sf.readline().decode('utf-8')
-    print(line)
+def play_music():
+    proc = Popen("/home/pi/music/play_music.sh")
+    print("process id = %s" % proc.pid)
+
+
+def stop_music():
+    proc = Popen("/home/pi/music/stop_music.sh")
+    print("process id = %s" % proc.pid)
+
+
+voice2cmd = {
+    "robot play music": play_music,
+    "robot stop music": stop_music,
+}
+
+try:
+    while True:
+        try:
+            result = client.results.get(False)
+        except Queue.Empty:
+            continue
+        if isinstance(result, pyjulius.Sentence):
+            text = str(result)
+            print('"%s" is detected' % text)
+            if text in voice2cmd:
+                voice2cmd[text]()
+            else:
+                print('"%s" is ignored' % text)
+except KeyboardInterrupt:
+    client.stop()
+    client.join()
+    client.disconnect()
