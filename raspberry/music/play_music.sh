67dae02c01a94b09830b0676393afcf0addfe206
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-11-10 21:34:49 -0800

    Add existence check of mplayer process

--- raspberry/music/play_music.sh
+++ raspberry/music/play_music.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check music is already playing
+PID=`ps -ef | grep mplayer | grep -v grep | awk '{print $2}'`
+if [ -n "${PID}" ]; then
+	echo "mplayer process(${PID}) already exist."
+	exit 0
+fi
+
 # Create music playlist
 if [ ! -f /home/pi/music/play.list ]; then
 	find /home/pi/music/hot-100/ -name "*.mp3" > /home/pi/music/play.list
