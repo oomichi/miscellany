a0300f7c60a0b3fd29f6b7258f98c75bec1d0249
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-11-16 05:48:43 -0800

    Drop stdout of say_something to avoid noise

--- raspberry/text2voice/say_something.sh
+++ raspberry/text2voice/say_something.sh
@@ -4,6 +4,6 @@ WORD=$1
 
 TEMPFILE=`mktemp /tmp/something-XXXX.wav`
 espeak -v en "${WORD}" -w ${TEMPFILE}
-mplayer -ao alsa:device=plughw=1.0 ${TEMPFILE}
+mplayer -ao alsa:device=plughw=1.0 ${TEMPFILE} > /dev/null
 rm -f ${TEMPFILE}
 
