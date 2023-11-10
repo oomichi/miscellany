6f6327e93f1b32b4708be1ea6bc04c55faef945a
Kenichi Omichi <ken1ohmichi@gmail.com>
2023-11-10 21:01:17 +0000

    Add container process

--- python/restapi-db/build_container_image.sh
+++ python/restapi-db/build_container_image.sh
@@ -7,6 +7,11 @@ if [ $? -ne 0 ]; then
 	sudo apt update
 	sudo apt install -y default-jre
 fi
+podman --version
+if [ $? -ne 0 ]; then
+	sudo apt update
+	sudo apt install -y podman
+fi
 
 if [ ! -f ./openapi-generator-cli.jar ]; then
 	wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.0.1/openapi-generator-cli-7.0.1.jar -O openapi-generator-cli.jar
@@ -19,3 +24,17 @@ java -jar openapi-generator-cli.jar generate --input-spec ./openapi-spec.yaml --
 
 # Update default_controller.py as we need
 cp ./default_controller.py output-by-generator/openapi_server/controllers/
+
+# This is a workaround. See https://github.com/oomichi/miscellany/issues/8#issuecomment-1806466051
+cp ./requirements.txt output-by-generator/
+
+set +e
+podman rmi --force openapi_server
+set -e
+
+cd output-by-generator/
+podman build -t openapi_server .
+
+echo "Succeeded to build the container image."
+echo "Run the following command to test the container image:"
+echo "$ podman run -p 8080:8080 openapi_server"
